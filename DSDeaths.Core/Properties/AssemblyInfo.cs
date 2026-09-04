6b12d307a8549399ccc3853bc8f94d9d07cf91d2
SaraEF <SaraEFCat334@protonmail.com>
2026-09-04 11:24:51 +0900

    Prepare v1.2.0-rc2 release (#3)
    Set release metadata to v1.2.0-rc2 and update the English/Japanese release documentation.

--- DSDeaths.Core/Properties/AssemblyInfo.cs
+++ DSDeaths.Core/Properties/AssemblyInfo.cs
@@ -9,4 +9,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("a63ef991-21d5-4773-98e2-df1462e327f6")]
 [assembly: AssemblyVersion("1.2.0.0")]
 [assembly: AssemblyFileVersion("1.2.0.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-rc1")]
+[assembly: AssemblyInformationalVersion("1.2.0-rc2")]
